5f2de51a6f23e0e89d5b442b89e83188d1b49e2c
Marc Rousavy <me@mrousavy.com>
2022-01-18 15:43:29 +0100

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { getArrayBuffer } from 'react-native-blob-jsi-helper';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
   React.useEffect(() => {
     (async () => {
       console.log('Fetching 8k image..');
@@ -16,8 +14,12 @@ export default function App() {
       const blob = await x.blob();
       console.log(`Blob: ${Object.keys(blob)}..`);
       console.log('Getting ArrayBuffer..');
+      // @ts-expect-error performance actually exists.
+      // eslint-disable-next-line no-undef
       const start = performance.now();
       const arrayBuffer = getArrayBuffer(blob);
+      // @ts-expect-error performance actually exists.
+      // eslint-disable-next-line no-undef
       const end = performance.now();
       console.log(
         `Got ArrayBuffer in ${end - start}ms! Size: ${arrayBuffer.byteLength}`
@@ -25,11 +27,7 @@ export default function App() {
     })();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+  return <View style={styles.container} />;
 }
 
 const styles = StyleSheet.create({
