74c8c5010ae2d39f9ef217bc8d12597eabd31c67
Marc Rousavy <me@mrousavy.com>
2022-01-18 12:46:08 +0100

    Create installer base

--- ios/BlobJsiHelper.mm
+++ ios/BlobJsiHelper.mm
@@ -1,19 +1,44 @@
 #import "BlobJsiHelper.h"
 #import "react-native-blob-jsi-helper.h"
+#import <React/RCTBlobManager.h>
+#import <React/RCTBridge+Private.h>
+#import <ReactCommon/RCTTurboModule.h>
+#import <jsi/jsi.h>
 
 @implementation BlobJsiHelper
 
 RCT_EXPORT_MODULE()
 
-// Example method for C++
-// See the implementation of the example module in the `cpp` folder
-RCT_EXPORT_METHOD(multiply:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                  withResolver:(RCTPromiseResolveBlock)resolve
-                  withReject:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(install)
 {
-    NSNumber *result = @(example::multiply([a floatValue], [b floatValue]));
-
-    resolve(result);
+  NSLog(@"Installing global.getArrayBufferForBlobId...");
+  RCTBridge* bridge = [RCTBridge currentBridge];
+  RCTCxxBridge* cxxBridge = (RCTCxxBridge*)bridge;
+  if (cxxBridge == nil) {
+    return @false;
+  }
+  
+  using namespace facebook;
+  
+  auto jsiRuntime = (jsi::Runtime*) cxxBridge.runtime;
+  if (jsiRuntime == nil) {
+    return @false;
+  }
+  auto& runtime = *jsiRuntime;
+  
+  auto func = jsi::Function::createFromHostFunction(runtime,
+                                                    jsi::PropNameID::forUtf8(runtime, "getArrayBufferForBlobId"),
+                                                    1,
+                                                    [](jsi::Runtime& runtime,
+                                                       const jsi::Value& thisArg,
+                                                       const jsi::Value* args,
+                                                       size_t count) -> jsi::Value {
+    return jsi::Value(5);
+  });
+  runtime.global().setProperty(runtime, "getArrayBufferForBlobId", func);
+  
+  NSLog(@"Installed global.getArrayBufferForBlobId!");
+  return @true;
 }
 
 @end
